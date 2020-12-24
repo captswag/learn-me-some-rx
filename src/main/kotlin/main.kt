13eebcc66c8851d2cff82edb886ac2d2334e7271
captswag <iamanjithsasindran@gmail.com>
2020-12-24 20:52:41 +0530

    Remove main.kt

--- src/main/kotlin/main.kt
+++
@@ -1,9 +0,0 @@
-import modals.RepositoryImpl
-
-fun main() {
-    val repository = RepositoryImpl()
-    repository.getChatMessages() // Without a take() operator, this will go on forever since getChatMessages is an interval
-        .blockingSubscribe { chat ->
-            println(chat.message)
-        }
-}
